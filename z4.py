b7a665895230fed982224ef32b1e37e65e9b7481
KFrankowski99 <93339540+KFrankowski99@users.noreply.github.com>
2023-05-24 16:44:11 +0200

    Add files via upload

---
+++ z4.py
@@ -0,0 +1,35 @@
+class Samochod:
+    def __init__(self, marka, model, rok_produkcji):
+        self.marka = marka
+        self.model = model
+        self.rok_produkcji = rok_produkcji
+
+    def podaj_zasieg(self):
+        pass
+
+
+class SamochodElektryczny(Samochod):
+    def __init__(self, marka, model, rok_produkcji, pojemnosc_baterii):
+        super().__init__(marka, model, rok_produkcji)
+        self.pojemnosc_baterii = pojemnosc_baterii
+
+    def podaj_zasieg(self):
+        return self.pojemnosc_baterii * 5
+
+
+class SamochodSpalinowy(Samochod):
+    def __init__(self, marka, model, rok_produkcji, pojemnosc_zbiornika):
+        super().__init__(marka, model, rok_produkcji)
+        self.pojemnosc_zbiornika = pojemnosc_zbiornika
+
+    def podaj_zasieg(self):
+        return self.pojemnosc_zbiornika * 15
+
+
+class SamochodHybrydowy(SamochodElektryczny, SamochodSpalinowy):
+    def __init__(self, marka, model, rok_produkcji, pojemnosc_baterii, pojemnosc_zbiornika):
+        super().__init__(marka, model, rok_produkcji, pojemnosc_baterii)
+        self.pojemnosc_zbiornika = pojemnosc_zbiornika
+
+    def podaj_zasieg(self):
+        return self.pojemnosc_baterii * 5 + self.pojemnosc_zbiornika * 15
